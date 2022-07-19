2f3dfe5c92a6a1f68d0e021789dff40983a8c42a
anishjain94 <jain.anish94@gmail.com>
2022-07-19 23:09:48 +0530

    :art: Webhook setup

---
+++ api/wehbooks.py
@@ -0,0 +1,38 @@
+from http.server import BaseHTTPRequestHandler
+import json
+import telegram
+import redis
+import os
+from telegram.ext import Updater
+from urllib.parse import urlparse, parse_qs
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        token = str(os.environ.get("botToken"))
+        updater = Updater(token)
+        updater.bot.setWebhook(
+            str(os.environ.get("webhookUrl"))
+        )
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        return
+
+    def do_POST(self):
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        res = json.loads(post_body)
+        print(res)
+
+        # send message to telegram
+        token = str(os.environ.get("botToken"))
+        updater = Updater(token)
+        updater.bot.send_message(
+            res["message"]["chat"]["id"], res["message"]["text"])
+        # telegram.Update.message.reply_to_message.text = res["message"]
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        return
--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-if __init__ == "__main__":
-    import sys
-    sys.path.append("../")
-    from src.main import main
-    main()
-    sys.exit(0)
+from http.server import HTTPServer
+from dotenv import load_dotenv
+import os
+import time
+from telegram.ext import Updater
+from api.wehbooks import handler
 
+load_dotenv()
+
+# HOST_NAME = os.environ.get("host", "localhost")
+# PORT_NUMBER = int(os.environ.get("port", 8000))
+
+HOST_NAME = "localhost"
+PORT_NUMBER = 8000
+
+if __name__ == '__main__':
+    server_class = HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), handler)
+    print(time.asctime(), f"Server Starts - {HOST_NAME}:{PORT_NUMBER}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print(time.asctime(), f"Server Stops - {HOST_NAME}:{PORT_NUMBER}")
